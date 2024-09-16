1da83a37443049048fc56bea13c354886ecc4d01
agilardino <alejandro.gilardino@gmail.com>
2024-09-16 16:55:41 -0500

    adding items
    items

---
+++ js/slide.js
@@ -0,0 +1,26 @@
+var images = ["quinoa-405538.jpg", "sacha.jpg", "quinoa-20.jpg"];
+
+var imageNumber = 0;
+var imageLength = images.length - 1;
+
+function changeImage(x) {
+    imageNumber += x;
+    
+    if (imageNumber > imageLength) {
+        imageNumber = 0;
+    }
+    if (imageNumber < 0) {
+        imageNumber = imageLength;
+    }
+    
+    document.getElementById("slideshow").src = images[imageNumber];
+    
+    return false;
+}
+
+
+
+
+function autoRun() {
+    setInterval("changeImage(1)", 8000);
+}
